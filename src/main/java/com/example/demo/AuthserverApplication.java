5dfb89c0eb50fcd131875f211daa34824f427486
Susitha Ravinda <susitha.ravinda@cinglevue.com>
2017-06-12 14:48:23 +0530

    Revert "persisting client details"
    This reverts commit fa666faa281c1960226287cfc458247de1e6c382.

--- src/main/java/com/example/demo/AuthserverApplication.java
+++ src/main/java/com/example/demo/AuthserverApplication.java
@@ -2,17 +2,25 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
 
 @SpringBootApplication
+@RestController
+@EnableAuthorizationServer
+@EnableResourceServer
 public class AuthserverApplication {
+
+    @RequestMapping("/user")
+    public Principal user(Principal user) {
+        return user;
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuthserverApplication.class, args);
 	}
-
-
 }
--- src/main/resources/data.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO oauth_client_details
-	(client_id, client_secret, scope, authorized_grant_types,
-	web_server_redirect_uri, authorities, access_token_validity,
-	refresh_token_validity, additional_information, autoapprove)
-VALUES
-	("acme", "acmesecret", "openid",
-	"authorization_code,refresh_token,password", null, null, 36000, 36000, null, true);
