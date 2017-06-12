fa666faa281c1960226287cfc458247de1e6c382
Susitha Ravinda <susitha.ravinda@cinglevue.com>
2017-06-12 11:59:22 +0530

    persisting client details

---
+++ src/main/java/com/example/demo/web/UserConnector.java
@@ -0,0 +1,16 @@
+package com.example.demo.web;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@RestController
+public class UserConnector {
+
+    @RequestMapping("/user")
+    public Principal user(Principal user) {
+        return user;
+    }
+}
